acaebd9db4a15e7dc14f042b892fcddbce09a3a7
Michael Wilson <pianoplayer868@hotmail.com>
2023-03-16 00:12:27 -0700

    Included MongoDB into the project and downloaded the middle ware leveraged for setting up the backend side of things.

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,66 @@
 const PORT = 8081;
 
-var express = require('express');
-var fs = require('fs');
-var cors = require('cors');
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 var app = express();
 app.use(cors());
+
+function createDeckCollection() {
+    const cardSchema = new mongoose.Schema(
+        {
+            name: String
+        }
+    );
+
+    const sectionSchema = new mongoose.Schema(
+        {
+            name: String,
+            cardList: [cardSchema]
+        }    
+    );
+
+    const deckSchema = new mongoose.Schema(
+        {
+            name: String,
+            commander: String,
+            sections: [sectionSchema]
+        }
+    );
+
+    const deckModel = mongoose.model('Deck', deckSchema);
+    deckModel.createCollection()
+    .then(collection => {
+        console.log('Deck collection has been created');
+    });
+}
+
+function connectToDatabase(data) {
+    const config = JSON.parse(data);
+    const uri = config.connectionString.replace('<password>', config.password);
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+
+    mongoose.connect(uri, connectionParams)
+    .then(() => {
+        console.log('Connected to the database');
+        createDeckCollection();
+    })
+    .catch(err => {
+        console.error(`Error connection to the database. \n${err}`);
+    });
+}
+
+// Connect to mongoDB
+fs.readFile(`${__dirname}/configs/server.json`, async (err, data) => {
+    if (err) throw err;
+
+    connectToDatabase(data);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -21,9 +76,7 @@ var server = app.listen(PORT, () => {
 app.get('/deckList/:deckName', (req, res) => {
     var name = req.params.deckName;
     fs.readFile(`${__dirname}/decks/${name}.json`, (err, data) => {
-        if (err) {
-            return console.error(err);
-        }
+        if (err) throw err;
 
         var deckList = JSON.parse(data);
         res.json(JSON.stringify(deckList));
