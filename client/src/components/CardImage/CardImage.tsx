44f6d11853107bb471a74fe0a9f56315641c589b
Michael Wilson <pianoplayer868@hotmail.com>
2023-04-03 15:01:01 +0900

    Added lazy loading to the project to improve site performance

--- client/src/components/CardImage/CardImage.tsx
+++ client/src/components/CardImage/CardImage.tsx
@@ -1,16 +1,19 @@
 import { CardConstants } from "../../utils/Constants";
 import { ICardProps } from "../../utils/Interfaces";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export const CardImage = ({card, size}: ICardProps) => {
     return (
-        <img
+        <LazyLoadImage
             // Curly braces enable JavaScript in markup
             src={card.imgUrl}
+            placeholderSrc={'../../images/placeholder.jgpeg'}
 
             // Curly braces can only be used as texxt or attributes
             alt={card.name}
             height={size ?? CardConstants.DEFAULT_CARD_HEIGHT}
             width={size ?? CardConstants.DEFAULT_CARD_WIDTH}
+            loading={'lazy'}
         />
     )
 }
